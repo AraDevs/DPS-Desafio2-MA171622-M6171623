6a3152c02c24a2233258878aa51b4ac58993a66e
Oscar Argueta <aradevs.argueta@gmail.com>
2020-09-30 17:32:08 -0600

    Added firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCPmRkPnDtnUPQVT_yLRz6dCaZhj4oOD7s",
+    authDomain: "dps-desafio.firebaseapp.com",
+    databaseURL: "https://dps-desafio.firebaseio.com",
+    projectId: "dps-desafio",
+    storageBucket: "dps-desafio.appspot.com",
+    messagingSenderId: "731114388737",
+    appId: "1:731114388737:web:4f0e85c15219995b416f00",
+    measurementId: "G-67KEZTFC4V"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCPmRkPnDtnUPQVT_yLRz6dCaZhj4oOD7s",
+    authDomain: "dps-desafio.firebaseapp.com",
+    databaseURL: "https://dps-desafio.firebaseio.com",
+    projectId: "dps-desafio",
+    storageBucket: "dps-desafio.appspot.com",
+    messagingSenderId: "731114388737",
+    appId: "1:731114388737:web:4f0e85c15219995b416f00",
+    measurementId: "G-67KEZTFC4V"
+  }
 };
 
 /*
